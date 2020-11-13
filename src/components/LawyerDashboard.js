53ddd63ea35ed63b36b5275956a44ee50073da05
Grzegorz-Malyska <vaversky.official@gmail.com>
2020-11-13 19:17:22 +0100

    beginning creating and styling lawyer dashboard

--- src/components/LawyerDashboard.js
+++ src/components/LawyerDashboard.js
@@ -1,7 +1,45 @@
 import React from "react";
+import "./LawyerDashboard.css";
 
 const LawyerDashboard = () => {
-    return <p>xyz</p>;
+    return (
+        // TODO: Add logout button (right upper corner)
+        <div>
+            <h1 className="text-center">Pdfpoint</h1>
+            <div className="action-btn-group">
+                <button>Upload PDF</button>
+                <button>Save PDF</button>
+            </div>
+
+            <div className="pdf-container">
+                <h2 className="text-center">Pdf</h2>
+                <canvas className="pdf-canvas" />
+            </div>
+            <div className="fields">
+                <h2 className="text-center">Fields</h2>
+                <ul className="fields-list">
+                    <li>
+                        <div className="field-item">
+                            Email
+                            <i />
+                        </div>
+                    </li>
+                    <li>
+                        <div className="field-item">
+                            Signature
+                            <i />
+                        </div>
+                    </li>
+                    <li>
+                        <div className="field-item">
+                            Checkbox
+                            <i />
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default LawyerDashboard;
